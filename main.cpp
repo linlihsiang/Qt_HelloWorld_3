6ca7822ccdcaaab59bef2650e38f5f4bfe94f386
linlihsiang <41343116@nfu.edu.tw>
2025-10-20 15:12:33 +0800

    Project Completed

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    DesignWidget w;
-    w.show();
+    DesignWidget w1,w2;
+    w1.show();
+    w1.move(300,300);
+    w2.show();
     return a.exec();
 }
